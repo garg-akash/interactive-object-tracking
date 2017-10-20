cc8937f8892282cd52f73fb3395891b16deb73ec
akash <akashgrg2@gmail.com>
2017-10-21 00:48:39 +0530

    objectTracking

---
+++ CMakeFiles/IntObjTrack.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/IntObjTrack.dir/IntObjTrack.cpp.o"
+  "IntObjTrack.pdb"
+  "IntObjTrack"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/IntObjTrack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
